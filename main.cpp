19f1b8876d91000b7ee9d023b786e95aa3e73a81
Gabriel Camargo <creeplays@gmail.com>
2020-03-31 00:45:21 -0300

    Tetris basic

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-#include <
+#include <thread>
+#include <vector>
 using namespace std;
+
+#include <stdio.h>
+#include <Windows.h>
 
 wstring tetromino[7];
 
 int nFieldWidth = 12;
 int nFieldHeight = 18;
+
+int nScreenWidth = 80;
+int nScreenHeight = 30;
 
 unsigned char *pField = nullptr;
 
@@ -22,9 +29,40 @@ int Rotate(int px, int py, int r)
     return 0;
 }
 
+bool DoesPieceFit(int nTetoromino, int nRotation, int nPosX, int nPosY)
+{
+    for (int px = 0; px < 4; px++)
+    {
+        for (int py = 0; py < 4; py++)
+        {
+            int pi = Rotate(px, py, nRotation);
+
+            int fi = (nPosY + py) * nFieldWidth + (nPosX + px);
+            if(nPosX + py >= 0 && nPosX + px < nFieldWidth)
+                if (nPosY + py >= 0 && nPosY + py < nFieldHeight)
+                {
+                    if (tetromino[nTetoromino][pi] == L'X' && pField[fi] != 0)
+                    {
+                        return false;
+                    }
+                }
+        }
+    }
+
+    return true;
+}
 
 int main()
 {
+    wchar_t *screen = new wchar_t[nScreenWidth * nScreenHeight];
+    for (int i = 0; i < nScreenWidth * nScreenHeight; i++)
+    {
+        screen[i] = L' ';
+    }
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
     //Criando peças/assets
     tetromino[0].append(L"..X.");
     tetromino[0].append(L"..X.");
@@ -65,13 +103,158 @@ int main()
     //Definindo as bbordas do campo
     for(int x = 0; x < nFieldWidth; x++)
     {
-        for(int y = 0; y < nFieldHeight; x++)
+        for(int y = 0; y < nFieldHeight; y++)
         {
-            pField[Y*nFieldWidth + x] = (x == 0 || x = nFieldWidth - 1 || y = nFieldHeight -1) ? 9 : 0
+            pField[y * nFieldWidth + x] = (x == 0 || x == nFieldWidth - 1 || y == nFieldHeight - 1) ? 9 : 0;
         }
-        wchar_t* screen = new wchar_t[nFieldWidth * nFieldHeight];
-        HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-        SetConsoleActiveScreenBuffer(hConsole);
-        DWORD dwBytesWritten = 0;
+    }
+
+    bool bGameOver = false;
+
+    int nCurrentPiece = 0;
+    int nCurrentRotation = 0;
+    int nCurrentX = nFieldWidth / 2;
+    int nCurrentY = 0;
+    
+    bool bKey[4];
+    bool bRotateLock = false;
+
+    int nSpeed = 20;
+    int nSpeedCounter = 0;
+
+    bool bForceDown = false;
+
+    int nLineCounter = 0;
+
+    vector<int> vLines;
+
+    while (!bGameOver)
+    {
+        // Controle de FPS
+        this_thread::sleep_for(50ms);
+        nSpeedCounter++;
+        bForceDown = (nSpeedCounter == nSpeed);
+
+        // Inputs
+        for (int k = 0; k < 4; k++)
+        {
+            bKey[k] = (0x8000 & GetAsyncKeyState((unsigned char)("\x27\x25\x28\x26"[k]))) != 0;
+        }
+
+        //Logica do tetris
+        nCurrentX += (bKey[0] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX + 1, nCurrentY)) ? 1 : 0;
+        nCurrentX -= (bKey[1] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX - 1, nCurrentY)) ? 1 : 0;
+        nCurrentY += (bKey[2] && DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1)) ? 1 : 0;
+
+        if (bKey[3])
+        {
+            nCurrentRotation += (!bRotateLock && DoesPieceFit(nCurrentPiece, nCurrentRotation + 1, nCurrentX, nCurrentY)) ? 1 : 0;
+            bRotateLock = true;
+        }
+        else
+        {
+            bRotateLock = false;
+        }
+
+        if (bForceDown)
+        {
+            if (DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY + 1))
+            {
+                nCurrentY++;
+            }
+            else
+            {
+                //Trava de peça na posição final
+                for (int px = 0; px < 4; px++)
+                {
+                    for (int py = 0; py < 4; py++)
+                    {
+                        if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] != L'.')
+                        {
+                            pField[(nCurrentY + py) * nFieldWidth + (nCurrentX + px)] = nCurrentPiece + 1;
+                        }
+                    }
+                }
+
+                for (int py = 0; py < 4; py++)
+                {
+                    if (nCurrentY + py < nFieldHeight - 1)
+                    {
+                        bool bLine = true;
+                        for (int px = 1; px < nFieldWidth - 1; px++)
+                        {
+                            bLine &= (pField[(nCurrentY + py) * nFieldWidth + px]) != 0;
+                        }
+
+                        if (bLine)
+                        {
+                            for (int px = 1; px < nFieldWidth - 1; px++)
+                            {
+                                pField[(nCurrentY + py) * nFieldWidth + px] = 8;
+                            }
+
+                            vLines.push_back(nCurrentY + py);
+                            nLineCounter++;
+
+                            if (nLineCounter >= 5)
+                            {
+                                nSpeed += (nSpeed >= 10) ? 0 : -1;
+                                nLineCounter - 5;
+                            }
+                        }
+                    }
+                }
+
+                nCurrentX = nFieldWidth / 2;
+                nCurrentY = 0;
+                nCurrentRotation = 0;
+                nCurrentPiece = rand() % 7;
+
+                bGameOver = !DoesPieceFit(nCurrentPiece, nCurrentRotation, nCurrentX, nCurrentY);
+            }
+            nSpeedCounter = 0;
+        }
+
+        // Desenho do campo :)
+        for (int x = 0; x < nFieldWidth; x++)
+        {
+            for (int y = 0; y < nFieldHeight; y++)
+            {
+                screen[(y + 2) * nScreenWidth + (x + 2)] = L" ABCDEFG=#"[pField[y * nFieldWidth + x]];
+            }
+        }
+
+        //Desenho da peça atual
+        for(int px = 0; px < 4; px++)
+        {
+            for (int py = 0; py < 4; py++)
+            {
+                if (tetromino[nCurrentPiece][Rotate(px, py, nCurrentRotation)] == L'X')
+                {
+                    screen[(nCurrentY + py + 2) * nScreenWidth + (nCurrentX + px + 2)] = nCurrentPiece + 65;
+                }
+            }
+        }
+        if (!vLines.empty())
+        {
+            WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+            this_thread::sleep_for(400ms);
+
+            for (auto& v : vLines)
+            {
+                for (int px = 1; px < nFieldWidth - 1; px++)
+                {
+                    for (int py = v; py > 0; py--)
+                    {
+                        pField[py * nFieldWidth + px] = pField[(py - 1) * nFieldWidth + px];
+                        pField[px] = 0;
+                    }
+                }
+            }
+            vLines.clear();
+        }
+
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
+
     }
 }
